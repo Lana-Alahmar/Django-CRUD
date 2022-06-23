4e032424495f77c6656fe55a458a5b3c4172ab8b
Lana-Alahmar <104502474+Lana-Alahmar@users.noreply.github.com>
2022-06-23 20:47:15 +0300

    comment

---
+++ LANA_ALAHMAR/blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from .models import Post
+
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostUpdateView(CreateView):
+    model = Post
+
+    fields = “__all__”
+
+    success_url  = reverse_lazy(“blog:all”)
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView)
+    model = Post
+
+    fields = “__all__”
+
+    success_url  = reverse_lazy(“blog:all”)
+
+class PostDeleteView (UpdateView)
+    model = Post
+
+    fields = “__all__”
+
+    success_url  = reverse_lazy(“blog:all”)
